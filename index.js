269b367139eccfdc115a577fed319eb7b426455a
OtabekUmarov <otabekumarov0220@gmail.com>
2022-06-07 09:24:58 +0500

    some

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.set('views', 'views')
 app.use(express.urlencoded({
     extended: true
 }))
-app.use(express.static('public'))
+app.use(express.static('assets'))
 
 // app.use(express.static(__dirname + '/public'))
 app.use('/media', express.static('media')) // !
